f4600582d4ccb80b05daccf7dfbb11ad78543963
Kevin Davis <kp.davis@outlook.com>
2021-11-30 15:41:53 -0500

    test: add unit test for season urls

---
+++ fastfantasy/tests/test_tournaments.py
@@ -0,0 +1,14 @@
+from fastfantasy.tournaments import EspnSeasonSchedule
+
+def test_season_urls():
+    """Test season urls from given season inputs (start and end)"""
+    expected = ['https://www.espn.com/golf/schedule/_/season/2017', 
+            'https://www.espn.com/golf/schedule/_/season/2018',
+            'https://www.espn.com/golf/schedule/_/season/2019']
+    
+    espn_ss = EspnSeasonSchedule(2017, 2019)
+    espn_ss.set_season_urls()
+
+    actual = espn_ss.get_season_urls()
+
+    assert actual == expected
