0299e5bfc4fb332990ff74fa3ae7d186deba3553
Kevin Davis <kp.davis@outlook.com>
2021-11-30 14:11:19 -0500

    feat: add espn tournament class

---
+++ fastfantasy/src/fastfantasy/tournaments.py
@@ -0,0 +1,68 @@
+
+
+class EspnTournament():
+
+    def __init__(self) -> None:
+        self.tournament_id = ""
+        self.tournament_name = ""
+        self.date = ""
+        self.purse = ""
+        self.winning_score = ""
+        self.winner_name = ""
+        self.winner_id = ""
+        self.season_id = ""
+
+    def get_tournament_id(self):
+        pass
+    
+    def set_tournament_id(self):
+        pass
+
+    def get_tournament_name(self):
+        pass
+
+    def set_tournament_name(self):
+        pass
+
+    def get_date(self):
+        pass
+    
+    def set_date(self):
+        pass
+
+    def get_purse(self):
+        pass
+
+    def set_purse(self):
+        pass
+
+    def get_winning_score(self):
+        pass
+
+    def set_winning_score(self):
+        pass
+
+    def get_winner_name(self):
+        pass
+
+    def set_winner_name(self):
+        pass
+
+    def get_winner_id(self):
+        pass
+
+    def set_winner_id(self):
+        pass
+
+    def get_season_id(self):
+        pass
+
+    def set_season_id(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    
+    main()
