5a62834bf4da5c00f76f90f72a5130789cc7e7aa
Kevin Davis <kp.davis@outlook.com>
2021-12-05 15:43:39 -0500

    added tournament participant class

---
+++ fastfantasy/src/fastfantasy/historical_data.py
@@ -0,0 +1,15 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+class TournamentParticipants():
+
+    def __init__(self) -> None:
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
