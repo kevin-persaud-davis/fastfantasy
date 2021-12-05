9f9362e5c45c7c9c9ee3af3c0c58ff893118f6c7
Kevin Davis <kp.davis@outlook.com>
2021-12-05 14:15:13 -0500

    added directories to reach path

--- fastfantasy/config/config.py
+++ fastfantasy/config/config.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 # Directories
 BASE_DIR = Path.home()
+ML_DIR = Path(BASE_DIR, "machine_learning")
+PROJECT_DIR = Path(ML_DIR, "projects")
+FASTFANTASY_DIR = Path(PROJECT_DIR, "fastfantasy")
