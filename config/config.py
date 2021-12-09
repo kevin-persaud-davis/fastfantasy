5b1f82c0d712c11f37a2c3bb01626e42a7f11038
Kevin Davis <kp.davis@outlook.com>
2021-12-09 14:05:44 -0500

    fixed file paths

--- config/config.py
+++ config/config.py
@@ -6,10 +6,10 @@ BASE_DIR = Path.home()
 ML_DIR = Path(BASE_DIR, "machine_learning")
 PROJECT_DIR = Path(ML_DIR, "projects")
 TOP_DIR = Path(PROJECT_DIR, "fastfantasy")
-FASTFANTASY_DIR = Path(TOP_DIR, "fastfantasy")
 
-CONFIG_DIR = Path(FASTFANTASY_DIR, "config")
-DATA_DIR = Path(FASTFANTASY_DIR, "data")
+
+CONFIG_DIR = Path(TOP_DIR, "config")
+DATA_DIR = Path(TOP_DIR, "data")
 
 RAW_DATA_DIR = Path(DATA_DIR, "raw")
 PROCESSED_DATA_DIR = Path(DATA_DIR, "processed")
