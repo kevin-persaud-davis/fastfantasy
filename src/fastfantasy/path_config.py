c10de1d56b9d1b9f5c7d3d44f248949416120659
Kevin Davis <kp.davis@outlook.com>
2021-12-09 14:44:54 -0500

    added paths

---
+++ src/fastfantasy/path_config.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+BASE = os.path.dirname(os.path.realpath(__file__))
+
+DATA = Path(BASE, "data")
+DATA_RAW = Path(DATA, "raw")
+DATA_PROCESSED = Path(DATA_RAW, "processed")
+
+
