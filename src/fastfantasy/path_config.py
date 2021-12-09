fb51b6e43b5be9c2a65d6e7c05e359b7cec27675
Kevin Davis <kp.davis@outlook.com>
2021-12-09 14:57:59 -0500

    fixed processed data path

--- src/fastfantasy/path_config.py
+++ src/fastfantasy/path_config.py
@@ -5,6 +5,6 @@ BASE = os.path.dirname(os.path.realpath(__file__))
 
 DATA = Path(BASE, "data")
 DATA_RAW = Path(DATA, "raw")
-DATA_PROCESSED = Path(DATA_RAW, "processed")
+DATA_PROCESSED = Path(DATA, "processed")
 
 
