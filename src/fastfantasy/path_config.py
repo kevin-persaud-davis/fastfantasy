2182fd934c958a715c97fa1994df84d762ddfe6a
Kevin Davis <kp.davis@outlook.com>
2021-12-09 19:37:09 -0500

    added new tournament data dirs

--- src/fastfantasy/path_config.py
+++ src/fastfantasy/path_config.py
@@ -4,7 +4,11 @@ from pathlib import Path
 BASE = os.path.dirname(os.path.realpath(__file__))
 
 DATA = Path(BASE, "data")
+
 DATA_RAW = Path(DATA, "raw")
+RAW_TOURNAMENTS = Path(DATA_RAW, "tournaments")
 DATA_PROCESSED = Path(DATA, "processed")
+PROCESSED_TOURNAMENTS = Path(DATA_PROCESSED, "tournaments")
 
-
+print(PROCESSED_TOURNAMENTS)
+print(RAW_TOURNAMENTS)
