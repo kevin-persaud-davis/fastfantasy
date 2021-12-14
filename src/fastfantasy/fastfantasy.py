f1a3c3749aba63d9679fc799adc69a8f477b90a6
Kevin Davis <kp.davis@outlook.com>
2021-12-14 15:49:56 -0500

    added raw data api function

---
+++ src/fastfantasy/fastfantasy.py
@@ -0,0 +1,31 @@
+from historical_data import DataRunner, merge_tournaments
+from draftkings_mappings import FantasyMapper, mapper_runner
+
+
+def raw_data(start_season, end_season=None, save_data=False):
+
+    if save_data:
+        if end_season is not None:
+
+            data_fn = f"hpd_{start_season}_{end_season}.csv"
+        else:
+            data_fn = f"hpd_{start_season}.csv"
+
+        merge_tournaments("*.csv", data_fn)
+    
+    else:
+        data_fn = "hpd.csv"
+        raw_df = merge_tournaments("*.csv", data_fn, save=False)
+        
+        return raw_df
+
+
+
+def main():
+    
+    df = raw_data(2018)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
