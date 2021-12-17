44432a5d9faf9aefce0d802d8560e42cde89966a
Kevin Davis <kp.davis@outlook.com>
2021-12-17 15:03:04 -0500

    cleaned up

--- tests/test_tournaments.py
+++ tests/test_tournaments.py
@@ -1,33 +1,9 @@
-from fastfantasy.tournaments import EspnSeasonSchedule
 from fastfantasy.tournament import EspnTournament
 
 import requests
 from bs4 import BeautifulSoup
 import pytest
 
-def test_season_urls():
-    """Test season urls from given season inputs (start and end)"""
-    expected = ['https://www.espn.com/golf/schedule/_/season/2017', 
-            'https://www.espn.com/golf/schedule/_/season/2018',
-            'https://www.espn.com/golf/schedule/_/season/2019']
-    
-    espn_ss = EspnSeasonSchedule(2017, 2019)
-    espn_ss.set_season_urls()
-
-    actual = espn_ss.get_season_urls()
-
-    assert actual == expected
-
-def test_season_urls_start():
-    """Test season urls for only start season"""
-    expected = ['https://www.espn.com/golf/schedule/_/season/2017']
-
-    espn_ss = EspnSeasonSchedule(2017)
-    espn_ss.set_season_urls()
-
-    actual = espn_ss.get_season_urls()
-
-    assert actual == expected
 
 def test_espn_tournament_id():
     """Test espn tournament id"""
